ad89d35f80e9fa6dfd2040bfadca976e7bfaf651
IT21253812 <99318205+IT21253812@users.noreply.github.com>
2023-05-14 14:52:55 +0530

    add feedback model

---
+++ app/src/main/java/com/example/jobslk/models/FeedbackModel.kt
@@ -0,0 +1,11 @@
+package com.example.jobslk. models
+
+class FeedbackModel (
+    var feedbackId: String? = null,
+    var name: String? = null,
+    var email: String? = null,
+    var feedback: String? = null,
+    var username: String? = null,
+    var password: String? = null,
+
+)
