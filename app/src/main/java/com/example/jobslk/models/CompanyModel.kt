894ee3ec616a984dd41367425d4f8eb660923247
IT21372094 <sahankulathunga25@gmail.com>
2023-05-14 14:07:32 +0530

    create company module

---
+++ app/src/main/java/com/example/jobslk/models/CompanyModel.kt
@@ -0,0 +1,12 @@
+package com.example.main.models
+
+ data class CompanyModel (
+    var comId: String? = null,
+    var comName: String? = null,
+    var country: String? = null,
+    var state: String? = null,
+    var email: String? = null,
+    var username: String? = null,
+    var password: String? = null,
+ )
+
